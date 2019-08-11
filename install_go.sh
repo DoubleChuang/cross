1187f646f690ccb7211498a82686ebf7912278d2
Double <ethan9141@gmail.com>
2019-08-11 16:05:23 +0000

    Fix Delete existing go folder

--- install_go.sh
+++ install_go.sh
@@ -1,6 +1,11 @@
 DL_NAME=go1.12.7.linux-armv6l.tar.gz
 DL=https://dl.google.com/go/$DL_NAME
+PREFIX=/usr/local/go
 curl -L $DL -o $DL_NAME
+if [ -d $PREFIX ]
+then
+	sudo rm $PREFIX -rf
+fi
 sudo tar -C /usr/local -xvf $DL_NAME
 #cat >> ~/.profile << 'EOF'
 #	export GOPATH=$HOME/.go_path
